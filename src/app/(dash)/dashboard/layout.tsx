2eda0e01fa69974d9cadd8397ca6a4e12bca0e02
asavor <74147411+asavor@users.noreply.github.com>
2023-04-15 22:46:42 +0100

    redirect to login if auth token is invalid

--- src/app/(dash)/dashboard/layout.tsx
+++ src/app/(dash)/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../../../../styles/index.css";
 import "react-toastify/dist/ReactToastify.css";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Dashboard | VyroAI",
@@ -11,5 +13,10 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const cookieStore = cookies();
+  if (!cookieStore.has("authorization")) {
+    redirect("/login");
+  }
+
   return <section>{children}</section>;
 }
