fee94078bfe2de367c73e640df7a00e2bad5a81d
asavor <74147411+asavor@users.noreply.github.com>
2023-04-18 21:54:49 +0100

    remove next-theme

--- src/components/darkLightMode/light-dark.tsx
+++ src/components/darkLightMode/light-dark.tsx
@@ -3,8 +3,6 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 import useDarkSide from "../darkLightMode/darkToggle";
 import { useState } from "react";
 
-import { useTheme } from "next-themes";
-
 export default function LightDarkModeButton() {
   const [colorTheme, setTheme] = useDarkSide();
   const [isDarkMode, setDarkMode] = useState(false);
